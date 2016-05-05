010c22bd2505c95b4dbf4cc7a5c27d3a119da2e1
PlatitudinousPlatypus <PlatitudinousPlatypus@users.noreply.github.com>
2016-05-05 09:52:24 -0500

    Basic Package Setup
    Set up the basic packages and started on a reference class.

---
+++ src/com/cs/Reference.java
@@ -0,0 +1,9 @@
+package com.cs;
+
+public class Reference 
+{
+	public static final int SUIT_SPADES = 0;
+	public static final int SUIT_HEARTS = 1;
+	public static final int SUIT_CLUBS = 2;
+	public static final int SUIT_DIAMONDS = 3;
+}
